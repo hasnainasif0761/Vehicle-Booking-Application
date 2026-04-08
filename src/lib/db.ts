40a47a18f8702ed88424b3ecc4ffb6973caf92e7
hasnainasif0761 <hasnainasif07610761@gmail.com>
2026-04-08 22:10:47 +0500

    Connect-MongoDb-Database

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,7 +1,32 @@
+import mongoose from "mongoose";
+
 const mongodbUrl = process.env.MONGODB_URL;
 
 if(!mongodbUrl){
     throw new Error('db url not found');
 }
 
-const cached = global.mongoosConn
+let cached = global.mongoosConn;
+if(!cached){
+    cached=global.mongoosConn={conn:null,promise:null};
+}
+
+const connectDb = async () => {
+    if(cached.conn){
+        return cached.conn
+    }
+
+    if(!cached.promise){
+        cached.promise = mongoose.connect(mongodbUrl).then(c=>c.connection)
+    }
+
+
+    try {
+        const conn = await cached.promise
+        return conn
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export default connectDb;
